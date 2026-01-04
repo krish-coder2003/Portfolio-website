bccbadecdba7ac012dbe150cacca6ef24f5438e8
krish-coder2003 <krishnashrangare@gmail.com>
2026-01-04 11:20:01 +0530

    Update website

--- src/components/ResumeSection.jsx
+++ src/components/ResumeSection.jsx
@@ -25,7 +25,7 @@ export default function ResumeSection() {
 
           <div className="resume-actions">
             <a
-              href="/krishna_shrangare.pdf"
+              href="/krishna shrangare Fresher.pdf"
               className="btn primary"
               target="_blank"
               rel="noreferrer"
@@ -33,7 +33,7 @@ export default function ResumeSection() {
               View Resume
             </a>
             <a
-              href="/krishna_shrangare.pdf"
+              href="/krishna shrangare Fresher.pdf"
               className="btn secondary"
               download
             >
